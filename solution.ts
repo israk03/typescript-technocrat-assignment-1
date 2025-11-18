4e9599a0ce06cd848998224a4a1f2a821ce48409
israk03 <israkchowdhury03@gmail.com>
2025-11-18 15:24:41 +0600

    Solve Problem 2: Implement getLength function using type guards for strings and arrays

--- solution.ts
+++ solution.ts
@@ -9,6 +9,17 @@ function formatValue(value: string | number | boolean): string | number | boolea
 
 }
 
-console.log(formatValue('hello'));
-console.log(formatValue(5));
-console.log(formatValue(true));
+
+
+const getLength = (value: string | any[]): number =>{
+    if(typeof value === "string"){
+        return value.length;
+    }else if(Array.isArray(value)){
+        return value.length;
+    }else{
+        return 0
+    }
+}
+
+console.log(getLength('typescript'));
+console.log(getLength([10, 20, 30, 40]));
