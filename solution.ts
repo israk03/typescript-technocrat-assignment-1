1ddac587d67876fc599a9a48d879935414771961
israk03 <israkchowdhury03@gmail.com>
2025-11-18 16:42:04 +0600

    Fix: Correct semicolon placement in some functions

--- solution.ts
+++ solution.ts
@@ -29,12 +29,12 @@ class Person {
     age: number;
 
     constructor(name: string, age: number){
-        this.name = name
-        this.age = age
+        this.name = name;
+        this.age = age;
     }
 
     getDetails(): string {
-        return `"Name:  ${this.name}, Age:  ${this.age}"`;
+        return `'Name:  ${this.name}, Age:  ${this.age}'`;
     }
 
 }
